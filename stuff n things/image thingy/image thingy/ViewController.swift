2c6831d65fcd609b1de10fdd141b5028ccd1cb0a
swerze <reedsonofwill@gmail.com>
2019-10-02 09:17:19 -0700

    sdfghjk

---
+++ stuff n things/image thingy/image thingy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  image thingy
+//
+//  Created by Reed Wilson on 10/2/19.
+//  Copyright © 2019 Reed Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ stuff n things/image thingy/image thingyTests/image_thingyTests.swift
@@ -0,0 +1,34 @@
+//
+//  image_thingyTests.swift
+//  image thingyTests
+//
+//  Created by Reed Wilson on 10/2/19.
+//  Copyright © 2019 Reed Wilson. All rights reserved.
+//
+
+import XCTest
+@testable import image_thingy
+
+class image_thingyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
