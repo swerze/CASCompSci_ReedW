56f6e906f4e7ab62e67cb48e77e98ffaa58af5e1
swerze <reedsonofwill@gmail.com>
2020-01-29 10:04:39 -0800

    layout deal
    gfds

---
+++ Layout Thingy/Layout Thingy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Layout Thingy
+//
+//  Created by Reed Wilson on 1/29/20.
+//  Copyright © 2020 Reed Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
